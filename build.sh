b4c60d7a79b11a237da2f49440493f33420f6031
shwewo <shwewo@gmail.com>
2024-09-05 08:48:57 +0300

    no actions :(

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd tdesktop
+for patch in ../*.patch; do
+  echo "Applying patch: $patch"
+  patch -p1 < "$patch"
+  echo -e ""
+done
+
+podman run --rm -it \
+  -v "$PWD:/usr/src/tdesktop" \
+  ghcr.io/telegramdesktop/tdesktop/centos_env:latest \
+  /usr/src/tdesktop/Telegram/build/docker/centos_env/build.sh \
+  -D TDESKTOP_API_ID=611335 \
+  -D TDESKTOP_API_HASH=d524b414d21f4d37f08684c1df41ac9c \
+  -D DESKTOP_APP_DISABLE_AUTOUPDATE=ON
--- patch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd tdesktop
-for patch in ../*.patch; do
-  echo "Applying patch: $patch"
-  patch -p1 < "$patch"
-  echo -e "\n"
-done
